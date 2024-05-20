2d9147a3e1cd69300c5088442113be0703bf4b7a
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-20 18:36:32 +0200

    Added js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const displayElm = document.querySelector('.display');
 
+const handleDigitClick = (event) => {
+  if (displayElm.textContent.length === 9) {
+    return;
+  }
+  const digit = event.target.textContent;
+  if (displayElm.textContent === '0') {
+    displayElm.textContent = digit;
+  } else {
+    displayElm.textContent += digit;
+  }
+};
+
+document.querySelectorAll('.btn-num').forEach((button) => {
+  button.addEventListener('click', handleDigitClick);
+});
